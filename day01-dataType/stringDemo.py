993ebd7d2378429aeff3025f2a971de54ab653b3
huldar <cbj1301541@163.com>
2018-12-18 14:56:18 +0800

    增加README.md 描述性介绍

--- day01-dataType/stringDemo.py
+++ day01-dataType/stringDemo.py
@@ -187,12 +187,121 @@ python 3中能用于字符串的函数
       3.1 如果是数字,判断您输入的数字是否大于100
 
 """
-inputString = input("请输入一个字符串:")
-print("您输入的字符串是" + inputString + "其中空格的数量是:" + inputString.count(' ') + "个.")
-trimString = inputString.strip()
-print("去掉空格后的字符串是:" + trimString)
+# inputString = input("请输入一个字符串:")
+# print("您输入的字符串是" + inputString + "其中空格的数量是:" + inputString.count(' ') + "个.")
+# trimString = inputString.strip()
+# print("去掉空格后的字符串是:" + trimString)
+#
+# if inputString.isdigit():
+#     print("您输入的字符串是数字!")
+#     if int(inputString) > 100:
+#         print("你输入的字符串大于100")
 
-if inputString.isdigit():
-    print("您输入的字符串是数字!")
-    if int(inputString) > 100:
-        print("你输入的字符串大于100")
+"""
+  1. 用字符串 * 运算符打印三角形
+    要求输入一个整数,此整数代表最长的一行星离左侧的字节数
+  如:
+    请输入离左侧的距离: 3
+  打印如下:
+      *
+     ***
+    *****
+   *******
+"""
+leftOffset = int(input("请输入一个整数:"))
+asteriskNumber = 1
+while leftOffset > 0:
+    print(' ' * leftOffset + '*' * asteriskNumber)
+    leftOffset -= 1
+    asteriskNumber += 2
+"""
+输入一个字符串,把输入的字符串中的空格全部去掉.打印出处理后的字符串的长度和字符串内容
+"""
+trimingStr = input("请输入一个字符串:")
+# 去掉字符串中的所有空格
+trimedStr = trimingStr.replace(' ', '')
+print("处理后的字符串是:%s,他的长度是:%s" % (trimedStr, len(trimedStr)))
+"""
+ 输入三行文字,让这三行文字在一个方框内居中显示
+    如输入(不要输入中文)
+    hello!
+    I'm studing python!
+    I like python
+     打印如下:
+   +---------------------+
+   |       hello!        |
+   | I'm studing python! |
+   |    I like python    |
+   +---------------------+
+"""
+# 定义输入的行数
+rowNum = int(input("请输入一个整数,代表行数:"))
+# 定义一个列表接收输入的句子
+rowList = []
+# 获取字符串最大的长度
+max_len = -1
+for i in range(rowNum):
+    sentence = input("请输入第%s句,总共%s句:" % (i + 1, rowNum))
+    rowList.append(sentence)
+    if max_len < len(sentence):
+        max_len = len(sentence)
+# 构建收尾句
+firstAndEnd = '+' + '-' * (max_len + 2) + '+'
+print(firstAndEnd)
+# 遍历输入的句子并输入出
+for sentence in rowList:
+    print("|", sentence.center(max_len), "|")
+print(firstAndEnd)
+
+# 字符串格式化表达式
+"""
+  作用:
+    生成一定格式的字符串
+  运算符:
+    %
+  语法格式:
+    格式字符串  % 参数值
+    或
+    格式字符串  % (参数值1, 参数值2, ...)
+  说明:
+    % 左侧为格式字符串
+    % 右侧为参数值,当有多个参数值是用括号() 括起来,并用逗号(,)分隔
+    格式化字符串中以%开头的为占位符,占位符的位置将用参数值替换
+  示例:
+    fmt = "name: %s, age: %d"
+    s = fmt % ('weimingze', 35)
+    print(s)  # name: weimingze, age: 35
+    "温度:__%d__" % 32
+----------------------------------------------------------------
+格式化字符串中的占位符和类型码
+   %s   字符串,使用 str(x) 将x转换为字符串
+   %r   字符串,使用 repr(x) 将x转换为字符串
+   %c   整数转为单个字符
+   %d   转为十进制整数
+   %o   转为八进制整数
+   %x   十六制进整数(字符a-f小写)
+   %X   十六制进整数(字符A-F大写)
+   %e   指数型浮点数(e小写), 如2.9e+10
+   %E   指数型浮点数(E大写), 如2.9E+10
+   %f, %F 浮点数(小数形式)
+   %g, %G  十进制浮点数或指数浮点自动转换
+   %%   等同于一个%字符
+
+占位符和类型码之间的格式化语法:
+  % [- + 0 宽度.精度] 类型码
+  -   : 左对齐
+  +   : 显示正号
+  0   : 左侧空白位置补零
+  宽度 : 整个字符串的字符个数
+  精度 : 保留小数点后多少位(默认6位)
+ 
+示例:
+  "%10d"  % 123    # '       123'
+  "%+10d" % 123    # '      +123'
+  "%-10d" % 123    # '123       '
+  "%10s"  % 'abc'  # '       abc'
+  "%-5s"  % 'abc'  # 'abc  '
+  "%010d" % 123    # '0000000123'
+  "%7.3f" % 3.141592535897932  # '  3.141'
+  "%07.2f" % 3.141592535897932  # '0003.14'
+"""
